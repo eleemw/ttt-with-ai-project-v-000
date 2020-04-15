5eb12d7a5abffa0aa66eac1ee0934c834851c4f0
Erica Murdock-Waters <ericahpa@gmail.com>
2020-04-15 22:48:12 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,7 +2,7 @@ module Players
   class Computer < Player
     def move(board)
       puts "Computer select move 1 - 9"
-      if valid_move
+      if board.taken?(5)
         5
       else
        rand(1..9).to_s
